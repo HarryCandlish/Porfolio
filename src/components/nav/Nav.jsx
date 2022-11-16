1ef52648d714b7fe8eb2091970aa776c9301cc6c
Harry Candlish <hcnd90@gmail.com>
2022-11-17 11:11:08 +1300

    onclick function working to display selected section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,14 +6,17 @@ import {BsBookFill} from 'react-icons/bs'
 import {FaHammer} from 'react-icons/fa'
 import {AiTwotonePhone} from 'react-icons/ai'
 
+import {useState} from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-        <a href="#"><AiTwotoneHome/></a>
-        <a href="#about"><FaUserAlt/></a>
-        <a href="#experience"><BsBookFill/></a>
-        <a href="#portfolio"><FaHammer/></a>
-        <a href="#contact"><AiTwotonePhone/></a>
+        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiTwotoneHome/></a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAlt/></a>
+        <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBookFill/></a>
+        <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FaHammer/></a>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiTwotonePhone/></a>
     </nav>
   )
 }
