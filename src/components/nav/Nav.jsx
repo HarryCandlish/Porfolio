7b7d62b3e0de4855c681140dbc8b845b59f49c7b
Harry Candlish <hcnd90@gmail.com>
2022-11-17 10:26:08 +1300

    imported icons for navbar, with icons linking to appropriate sections

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiTwotoneHome} from 'react-icons/ai'
+import {FaUserAlt} from 'react-icons/fa'
+import {BsBookFill} from 'react-icons/bs'
+import {FaHammer} from 'react-icons/fa'
+import {AiTwotonePhone} from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+        <a href="#"><AiTwotoneHome/></a>
+        <a href="#about"><FaUserAlt/></a>
+        <a href="#experience"><BsBookFill/></a>
+        <a href="#portfolio"><FaHammer/></a>
+        <a href="#contact"><AiTwotonePhone/></a>
+    </nav>
   )
 }
 
