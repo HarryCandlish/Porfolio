3e9662d9bc43d24beb2bfa1358856098a61a18ed
Harry Candlish <hcnd90@gmail.com>
2022-11-17 15:31:28 +1300

    styles to experience section, changed coloring on about hovering

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,34 +13,39 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience_content">
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>JavaScript</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>Bootstrap</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>React</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>Workflow</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>Teams</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>Gatsby</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
           </div>
         </div>
@@ -51,34 +56,39 @@ const Experience = () => {
         <h3>Backend Development</h3>
         <div className="experience_content">
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>RestfulAPIs</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>JavaScript</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>Bootstrap</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>React</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>Headless CMS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>Workflow</h4>
-              <small className="text-light">Experienced</small>
-            </article>
-            <article className="experience_details">
-              <BsCheckCircleFill/>
-              <h4>Teams</h4>
-              <small className="text-light">Experienced</small>
+              <BsCheckCircleFill className="experience_details-icon"/>
+              <div>
+                <h4>GraphQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
           </div>
           </div>
