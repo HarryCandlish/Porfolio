ed2bffd77b7f87aaa62b1db21b52b34309f37c7d
Harry Candlish <hcnd90@gmail.com>
2022-11-18 08:33:57 +1300

    added in content for contacts section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './contact.css'
+import {MdEmail} from 'react-icons/md'
+import {AiTwotonePhone} from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className='contact_option'>
+          <MdEmail/>
+            <h4>Email</h4>
+            <h5>hcnd90@gmail.com</h5>
+            <a href="mailto:hcnd90@gmail.com">Send a Message</a>
+            </article>
+            <article className='contact_option'>
+          <AiTwotonePhone/>
+            <h4>Phone</h4>
+            <h5>027 392 9011</h5>
+            </article>
+        </div>
+      </div>
+    </section>
   )
 }
 
