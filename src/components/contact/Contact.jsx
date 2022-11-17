f3a20a350aec6d645bca0cf8fbf54d5215257323
Harry Candlish <hcnd90@gmail.com>
2022-11-18 08:53:14 +1300

    styling for contact list and form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,17 +12,24 @@ const Contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className='contact_option'>
-          <MdEmail/>
+          <MdEmail className="contact_option-icon"/>
             <h4>Email</h4>
             <h5>hcnd90@gmail.com</h5>
             <a href="mailto:hcnd90@gmail.com">Send a Message</a>
             </article>
             <article className='contact_option'>
-          <AiTwotonePhone/>
+          <AiTwotonePhone className="contact_option-icon"/>
             <h4>Phone</h4>
             <h5>027 392 9011</h5>
             </article>
         </div>
+
+        <form action="">
+          <input type="text" name='name' placeholder='Name' required />
+          <input type="email" name='email' placeholder='Email' required />
+          <textarea name="message" rows="7" placeholder='Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Submit Message</button>
+        </form>
       </div>
     </section>
   )
